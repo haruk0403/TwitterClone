fba3e9051b15262876a762b27f1fb8f995d57443
haruk0403 <haruka_rocky322@yahoo.co.jp>
2022-05-29 14:12:30 +0900

    つぶやき投稿機能

--- Views/post.php
+++ Views/post.php
@@ -1,37 +1,28 @@
-<?php
-// 設定関連を読み込む
-include_once('../config.php');
-// 便利な関数を読み込む
-include_once('../util.php');
-
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
  
 <head>
     <?php include_once('../Views/common/head.php'); ?>
-
     <title>つぶやく画面 / Twitterクローン</title>
     <meta name="description" content="つぶやく画面です">
 </head>
  
 <body class="home">
     <div class="container">
-     <?php include_once('../Views/common/side.php'); ?>   
+        <?php include_once('../Views/common/side.php'); ?>
         <div class="main">
             <div class="main-header">
                 <h1>つぶやく</h1>
             </div>
-
+ 
             <!-- つぶやき投稿エリア -->
-            <div class="tweet-area">
+            <div class="tweet-post">
                 <div class="my-icon">
-                    <img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt="">
+                    <img src="<?php echo htmlspecialchars($view_user['image_path']); ?>" alt="">
                 </div>
                 <div class="input-area">
-                    <form action="post.php" method="post" enctype="multipart/form-date">
-                        <textarea name="body" placeholder="いまどうしてる？" maxrength="140"></textarea>
+                    <form action="post.php" method="post" enctype="multipart/form-data">
+                        <textarea name="body" placeholder="いまどうしてる？" maxlength="140"></textarea>
                         <div class="bottom-area">
                             <div class="mb-0">
                                 <input type="file" name="image" class="form-control form-control-sm">
@@ -41,13 +32,12 @@ include_once('../util.php');
                     </form>
                 </div>
             </div>
+ 
             <!-- 仕切りエリア -->
             <div class="ditch"></div>
-
-                    </div>
-                </div>
-            </div>
-  <?php include_once('../Views/common/foot.php'); ?>         
+        </div>
+    </div>
+    <?php include_once('../Views/common/foot.php'); ?>
 </body>
  
 </html>
